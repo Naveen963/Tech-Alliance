b384efc64edb28ac22d14d0eb4c5e89653c18095
Naveen <naveenkumar200101@gmail.com>
2023-11-05 15:41:16 +0530

    TeamTA page social handles added

--- src/components/Team/TeamMember.jsx
+++ src/components/Team/TeamMember.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 import PropType from 'prop-types'
-
+import { BsLinkedin } from 'react-icons/bs'
+import { AiOutlineInstagram, AiOutlineGithub } from 'react-icons/ai'
 const MemberBlock = styled.div`
   font:'Poppins';
   font-weight: 600;
   display: grid;
-  grid-template-columns: 1fr 2.2fr;
+  grid-template-columns: 1fr 2.2fr 0.2fr;
   margin-top:5rem;
   overflow: hidden;
   border-radius: 8px;
@@ -41,6 +42,7 @@ const Details = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
+   
     padding: 0 2rem;
     gap:1rem;
    
@@ -61,8 +63,13 @@ color: var(--color-grey-600);
 font-family: 'Sono';
 `;
 
+
 const TeamMember = ({ member }) => {
   const { name, role, email, imageURL, phoneNo } = member
+
+  const socailLinkHandler = (url) => {
+    window.open(url);
+  }
   return (
     <MemberBlock>
 
@@ -73,8 +80,13 @@ const TeamMember = ({ member }) => {
         <Detail><i style={{ color: 'var(--color-grey-400)' }}>Email</i>: {email}</Detail>
         <Detail><i style={{ color: 'var(--color-grey-400)' }}>Contact</i>: {phoneNo}</Detail>
       </Details>
+      <Details style={{ alignItems: 'center' }} >
 
-    </MemberBlock>
+        <BsLinkedin color='#0A66C2' size={30} onClick={() => socailLinkHandler(member?.linkedInURL)} />
+        <AiOutlineInstagram color='#E4405F' size={40} onClick={() => socailLinkHandler(member?.instagramURL)} />
+        <AiOutlineGithub size={40} onClick={() => socailLinkHandler(member?.githubURL)} />
+      </Details>
+    </MemberBlock >
   )
 }
 
